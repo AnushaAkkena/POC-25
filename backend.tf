32fbc3fd44fb3fb605c5bf017015b821b3600749
AnushaAkkena <120028427+AnushaAkkena@users.noreply.github.com>
2026-01-21 20:02:39 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-968402771136-poc29-ap-south-1"
+    key     = "project29/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
+}
+
